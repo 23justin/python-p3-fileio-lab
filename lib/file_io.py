7aa2addbb4de929d7298006c1c79850714f6fe1e
14justin <justinmwangi40@gmail.com>
2024-06-03 23:17:41 +0300

    test

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
 def write_file(file_name, file_content):
     pass
+    with open(f'{file_name}.txt', 'w') as f:
+        f.write(file_content)
+        f.close()
 
 def append_file(file_name, append_content):
     pass
-
+    with open(f'{file_name}.txt', 'a') as f:
+        f.write(append_content)
+        f.close()
 def read_file(file_name):
-    pass
+    try:
+        with open(f'{file_name}.txt', 'r') as f:
+            read_content = f.read()
+        return read_content
+    except FileNotFoundError:
+        return f"File {file_name}.txt not found."
+    except IOError as e:
+        return f"An error occurred: {e}"
